74fbbf658b313224739d25b50eca80e56f11b4bf
Phil Pepin <ppepin34@gmail.com>
2022-05-08 17:52:52 -0400

    intial testing for Employee object

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,35 @@
+const { default: TestRunner } = require('jest-runner');
+const Employee = require('../lib/Employee');
+
+test('create new employee object', () => {
+    const employee = new Employee('Dave');
+
+    expect(employee.name).toBe('Dave');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+    expect(employee.role).toBe('Employee');
+})
+
+test("get employee's name", () => {
+    const employee = new Employee('Dave');
+
+    expect(employee.getName()).toEqual(expect.any(String))
+})
+
+test("get employee's ID", () => {
+    const employee = new Employee('Dave');
+    
+    expect(employee.getId()).toEqual(expect.any(Number));
+})
+
+test("get employee's email", () => {
+    const employee = new Employee('Dave');
+
+    expect(employee.getEmail()).toEqual(expect.any(String))
+})
+
+test("get employee's role", () => {
+    const employee = new Employee('Dave');
+
+    expect(employee.getRole()).toBe('Employee')
+})
