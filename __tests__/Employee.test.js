52b0dc39a96b3e7957d23db8baae1d63e06fa559
Phil Pepin <ppepin34@gmail.com>
2022-05-08 20:19:53 -0400

    manager test completes

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,7 +4,7 @@ const Employee = require('../lib/Employee');
 test('create new employee object', () => {
     const employee = new Employee('Dave');
 
-    expect(employee.name).toBe('Dave');
+    expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
     expect(employee.role).toBe('Employee');
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -3,7 +3,13 @@ const Manager = require('../lib/Manager');
 test('creates a manager object from an employee object', () => {
     const manager = new Manager('Dave');
 
-    expect(manager.name).toBe('Dave');
-    expect(manager.office).toBe('1');
+    expect(manager.name).toEqual(expect.any(String));
+    expect(manager.office).toEqual(expect.any(String));
     expect(manager.role).toBe('Manager');
+})
+
+test("get manager's role", () => {
+    const manager = new Manager('Dave');
+
+    expect(manager.role).toBe('Manager')
 })
